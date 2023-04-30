6c68dca82d61130c98f076e74a0248387de185f1
adwaith <adwaithkj@gmail.com>
2023-04-30 13:39:05 +0530

    image as json is wokring

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+import matplotlib.pyplot as plt
+from flask_cors import CORS
+from tensorflowpredict import loadeverything
 
 app= Flask(__name__)
+app.debug=True
+
+CORS(app, resources={r"/*": {"origins": "*"}})
+
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db' #three slashes for relative url and 4 slashes for absolute url
 
 db =SQLAlchemy(app)
@@ -23,9 +29,56 @@ def index():
         pass
     else:
         return render_template('index.html')
-
-
     
+image=[]
+result=[]
+
+
+@app.route("/upload",methods=["GET","POST","OPTIONS"])
+def upload():
+
+    headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'POST',
+    'Access-Control-Allow-Headers': 'Content-Type'}
+    # return "hello",200,headers
+    if request.method == 'OPTIONS':
+        print("options")
+        # Handle preflight request
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type'
+        }
+        return '', 200, headers
+    else:
+        # Handle actual request
+        image.append(request.json.get('image'))
+
+
+        print(image)
+        # Process the selected image here
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type'
+        }
+        print(image.dtype)
+        return jsonify({'success': True}), 200,headers
+
+
+@app.route("/view",methods=["GET","POST","OPTIONS"])
+def view():
+    # print(image)
+    if len(image)>0:
+        return image[-1]
+     
+
+@app.route("/predict",methods=["GET","POST","OPTIONS"])
+def predict():
+    loadeverything()
+    
+
 
 
 if __name__=="__main__":
